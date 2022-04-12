f0f44b1841933e0f8eabf781813735ed8e35b6a3
avivanguyen <aviva.nguyen@gmail.com>
2022-04-12 14:52:50 -0600

    update

---
+++ About/index.js
@@ -0,0 +1,7 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).click(function(){
+    $("button").mouseenter(function(){
+      alert("Your message has been sent and I'll contact you back.");
+    });
+  });
